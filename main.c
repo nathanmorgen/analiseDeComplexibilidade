ee01c69d331dfa186da87eaebf1338f6e8f016a0
Nataniel Fonseca <nataniel.fonseca@aluno.ufca.edu.br>
2022-12-05 21:51:35 -0300

    O codigo agora faz todos os testes  do bubblesort

--- main.c
+++ main.c
@@ -24,18 +24,21 @@ int main(void){
 
     clock_t tempo;
     clock_t aux = 0;
-    int tamanhoVetor = 10;
-    
-    for(int i = 0; i < 10; i++){
-        
-        int* vetor = criaVetor(tamanhoVetor);
-        //imprime(v, 10);
-        tempo = clock();
-        bubblesort(vetor, tamanhoVetor);
-        tempo = clock() - tempo;
-        aux += tempo;
-        tempo = 0;
-        free(vetor);
+    printf("Teste para o bubblesort\n\n");
+    for(int i = 10; i <= 10000; i=i*10){
+        printf("Vetor tamanho %d:\n",i);
+        for(int j = 0; j < 10; j++){
+            //printf("i = %d\n",j);
+            int* vetor = criaVetor(i);
+            //imprime(v, 10);
+            tempo = clock();
+            bubblesort(vetor, i);
+            tempo = clock() - tempo;
+            aux += tempo;
+            tempo = 0;
+            free(vetor);
+        }
+        printf("Tempo de execucao: %lf milissegundos\n", ((double)aux)/((CLOCKS_PER_SEC/1000)));
+        aux = 0;
     }
-    printf("Tempo de execucao: %lf milissegundos", ((double)aux)/((CLOCKS_PER_SEC/1000)));
 }
