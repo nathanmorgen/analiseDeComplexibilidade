7c49a8d93f20c9612be9b548879fe36b0db17dd7
Nataniel Fonseca <nataniel.fonseca@aluno.ufca.edu.br>
2022-12-10 17:41:34 -0300

    codigo concluido

--- main.c
+++ main.c
@@ -7,38 +7,38 @@
 int* criaVetor(int tamanho) {
     int *v = malloc(tamanho * sizeof(int));
 
-    //srand(time(NULL));
     srand((unsigned)time(NULL));
     for(int i = 0; i < tamanho; i++){
         v[i] = rand();
     }
     return v;
 }
-
+/*
+//funcao criada apenas para motivos de teste
 void imprimeVetor(int* v, int tamanho){
     for (int j = 0; j < tamanho; j++) printf("(%d, %d)\n", j, v[j]);
     free(v);
 }
-
+*/
 int main(void){
 
     clock_t tempo;
     clock_t aux = 0;
-    printf("Teste para o bubblesort\n\n");
+    printf("Teste para o Bubble Sort\n\n");
     for(int i = 10; i <= 10000; i=i*10){
         printf("Vetor tamanho %d:\n",i);
         for(int j = 0; j < 10; j++){
-            //printf("i = %d\n",j);
             int* vetor = criaVetor(i);
-            //imprime(v, 10);
             tempo = clock();
+            //aqui eu simplesmente troquei a funcao para cada teste e compilei, para evitar aumentar a complexidade do codigo.
             bubblesort(vetor, i);
+            //mergeSort(vetor, 0, i-1);
             tempo = clock() - tempo;
             aux += tempo;
             tempo = 0;
             free(vetor);
         }
-        printf("Tempo de execucao: %lf milissegundos\n", ((double)aux)/((CLOCKS_PER_SEC/1000)));
+        printf("Tempo de medio de execucao: %lf milissegundos\n", ((double)aux/10)/((CLOCKS_PER_SEC/1000)));
         aux = 0;
     }
 }
